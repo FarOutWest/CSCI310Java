ceb1495e1a293638e67a3790a40b58ab7ae88efe
FarOutWest <westDcastro@gmail.com>
2016-10-11 11:46:49 -0600

    hello world

---
+++ src/csci310java/CSCI310Java.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author West
+ */
+package csci310java;
+
+import static kiss.API.*;
+
+public class CSCI310Java {
+    void testRun() {
+        outExpect("Hello World!");
+        run();
+        outClose();
+    }
+    
+    void run() {
+        println("Hello World!");
+    }
+}
