202c4536eaeb3786ef095c08beb89b8362b2d6c7
FarOutWest <westDcastro@gmail.com>
2016-10-12 14:16:35 -0600

    hello world working and can run file

---
+++ src/collections/Collections.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections;
+
+import static kiss.API.outClose;
+import static kiss.API.outExpect;
+import static kiss.API.println;
+
+/**
+ *
+ * @author West
+ */
+public class Collections {
+        void testRun() {
+        outExpect("Hello World!");
+        run();
+        outClose();
+    }
+    
+    void run() {
+        println("Hello World!");
+    }
+}
