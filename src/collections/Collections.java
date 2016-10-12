f91cc3d1688b15545e43dd87b20e37e99a251492
FarOutWest <westDcastro@gmail.com>
2016-10-12 15:54:21 -0600

    added emp class with methods and collection of emp ids with everything working except for the getids method

--- src/collections/Collections.java
+++ src/collections/Collections.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package collections;
 
 import java.io.IOException;
@@ -29,6 +24,7 @@ import static kiss.API.println;
  * @author West
  */
 public class Collections {
+
     void testHashSet() {
         Set<String> names = new HashSet<String>();
 
@@ -61,10 +57,10 @@ public class Collections {
             }
         }
     }
-    
+
     void testLinkedList() {
         List<Integer> numbers = new LinkedList<Integer>();
-        
+
         boolean pass = false;
         try {
             println(numbers.get(0));
@@ -72,14 +68,14 @@ public class Collections {
             pass = true;
         }
         assert pass == true;
-        
+
         numbers.add(9);
         numbers.add(13);
         numbers.add(16);
         numbers.add(18);
 
-        assert numbers.get(numbers.size()-1).equals(18);
-        
+        assert numbers.get(numbers.size() - 1).equals(18);
+
         int[] a = new int[4];
         a[0] = 9;
         a[1] = 10;
@@ -116,5 +112,77 @@ public class Collections {
             }
         }
     }
+
+    class Emp {
+        Collection<Integer> ids = new LinkedList<Integer>();
+        int id;
+        String name;
+
+        void setids(Collection<Integer> ids){
+            ids.add(1001);
+            ids.add(2002);
+            ids.add(3003);
+            ids.add(4004);
+        }
+        
+        void setid(int _id) {
+            id = _id;
+        }
+
+        void setname(String _name) {
+            name = _name;
+        }
+
+        Collection<Integer> getids(Collection<Integer> ids) {
+            return ids;
+        }
+        
+        int getid() {
+            return id;
+        }
+
+        String getname() {
+            return name;
+        }
+    }
+
+    void testsetids(Collection<Integer> ids) {
+        try (Close out = outExpect(1001, EOL, 2002, EOL, 3003, EOL, 4004, EOL)) {
+            for (Integer value : ids) {
+                println(value);
+            }
+        }    
+    }
     
+    void testgetids(Collection<Integer> ids) {
+        //ids.getids(ids);
+    }
+    
+    void testsetid() {
+        Emp emp1 = new Emp();
+        Emp emp2 = new Emp();
+        Emp emp3 = new Emp();
+
+        emp1.setid(1001);
+        emp2.setid(2002);
+        emp3.setid(3003);
+        
+        assert emp1.getid() == 1001;
+        assert emp2.getid() == 2002;
+        assert emp3.getid() == 3003;
+    }
+    
+    void testsetname() {
+        Emp emp1 = new Emp();
+        Emp emp2 = new Emp();
+        Emp emp3 = new Emp();
+
+        emp1.setname("Luke");
+        emp2.setname("Vader");
+        emp3.setname("George");
+        
+        assert emp1.getname() == "Luke";
+        assert emp2.getname() == "Vader";
+        assert emp3.getname() == "George";
+    }
 }
