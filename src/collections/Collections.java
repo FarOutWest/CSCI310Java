a5cd63f15ad7ace4ee3c2e59dfbab1fbfd96332a
FarOutWest <westDcastro@gmail.com>
2016-10-13 11:14:58 -0600

    run working and compiling

--- src/collections/Collections.java
+++ src/collections/Collections.java
@@ -5,21 +5,6 @@
  */
 package collections;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
-import kiss.API.Close;
-import static kiss.API.EOL;
 import static kiss.API.outClose;
 import static kiss.API.outExpect;
 import static kiss.API.println;
