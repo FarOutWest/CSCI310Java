d4c575431fa31ab01bb14f9d1c37010b58763863
FarOutWest <westDcastro@gmail.com>
2016-10-28 16:21:22 -0600

    initialized testing using Junit

---
+++ test/collections/CollectionsTest.java
@@ -0,0 +1,35 @@
+package collections;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author West
+ */
+public class CollectionsTest {
+    
+    public CollectionsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
