f0eab274f1273d78250926d788cab05d91032226
FarOutWest <westDcastro@gmail.com>
2016-10-28 16:17:43 -0600

    added basic linked list with working functionality

--- src/collections/Collections.java
+++ src/collections/Collections.java
@@ -1,16 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package collections;
 
 import java.util.*;
 
 public class Collections {
-    void run() {
-        System.out.println("Hello World");
+    public static void main(String args[]) {
+        LinkedList<String> names = new LinkedList<String>();
+
+        names.add("Luke");
+        names.add("Han");
+        names.add("Chewie");
+        names.add("R2-D2");
+
+        System.out.println("Linked List Content: " + names);
+
+        names.addFirst("Vader");
+        names.addLast("Boba Fett");
+        System.out.println("LinkedList Content after addition: " + names);
+
+        Object var = names.get(0);
+        System.out.println("First element: " + var);
+        names.set(0, "Lando");
+        Object var2 = names.get(0);
+        System.out.println("First element after update by set method: " + var2);
+
+        names.removeFirst();
+        names.removeLast();
+        System.out.println("LinkedList after deletion of first and last element: " + names);
+
+        names.add(0, "C3PO");
+        names.remove(2);
+        System.out.println("Final Content: " + names);
     }
-    
-    
 }
