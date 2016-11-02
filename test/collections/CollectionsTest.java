7bcd468f14e901786c695079db4998ae586d802c
FarOutWest <westDcastro@gmail.com>
2016-11-02 08:43:47 -0600

    added tests to testing file

--- test/collections/CollectionsTest.java
+++ test/collections/CollectionsTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.*;
 
 /**
  *
@@ -32,8 +33,30 @@ public class CollectionsTest {
     public void tearDown() {
     }
     
-    public void testsomething() {
-        
+    public void testupdatefirst(LinkedList<String> list, String name) {
+        assert list.get(0).equals(name);
     }
     
+    public void testupdatelast(LinkedList<String> list, String name) {
+        assert list.getLast().equals(name);
+    }
+    
+    public void testremovefront (LinkedList<String> list) {
+        String temp = list.get(0);
+        assert !temp.equals(list.get(0));
+    }
+    
+    public void testremoveback (LinkedList<String> list) {
+        String temp = list.getLast();
+        assert !temp.equals(list.getLast());
+    }
+    
+    public void testaddindex(LinkedList<String> list, int num, String name) {
+        assert list.get(num).equals(name);
+    }
+    
+    public void testremoveindex(LinkedList<String> list, int num) {
+        String temp = list.get(num);
+        assert !list.get(num).equals(list.get(num));
+    }
 }
