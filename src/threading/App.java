f26145acebfb7e669d9eaaeeb88f420c62abf335
FarOutWest <westDcastro@gmail.com>
2016-11-03 11:32:34 -0600

    added Worker class as a thread/formatting

--- src/threading/App.java
+++ src/threading/App.java
@@ -3,22 +3,24 @@ package threading;
 import static kiss.API.*;
 
 /**
- *
  * @author West
  */
 public class App {
-    class Worker implements Runnable {
 
+    class Worker implements Runnable {
         @Override
         public void run() {
             println("Worker started.");
             pause(3.0);
             println("Worker done.");
         }
-        
     }
+
     void run() {
-        new Thread(()->{ pause(2.0); println("hi!"); }).start(); //lambda
+        new Thread(() -> {
+            pause(2.0);
+            println("hi!");
+        }).start(); //lambda
         Worker worker = new Worker();
         Thread workerThread = new Thread(worker);
         workerThread.start();
