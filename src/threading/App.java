842ffd94c661b88367296692e898289eb99c151f
FarOutWest <westDcastro@gmail.com>
2016-11-03 11:21:49 -0600

    added Worker class as a thread

--- src/threading/App.java
+++ src/threading/App.java
@@ -7,8 +7,22 @@ import static kiss.API.*;
  * @author West
  */
 public class App {
+    class Worker implements Runnable {
+
+        @Override
+        public void run() {
+            println("Worker started.");
+            pause(3.0);
+            println("Worker done.");
+        }
+        
+    }
     void run() {
-        new Thread(()->{ pause(2); println("hi!"); }).start();
+        new Thread(()->{ pause(2.0); println("hi!"); }).start(); //lambda
+        Worker worker = new Worker();
+        Thread workerThread = new Thread(worker);
+        workerThread.start();
+        //new Thread(new Worker()).start(); same as the three lines above so personal choice here
         ConsoleClock clock = new ConsoleClock();
         clock.start();
         //clock.run(); would casue the thread to never exit on ConsoleClock thus printing the same thing forever
